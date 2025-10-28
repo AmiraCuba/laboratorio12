54c758b5a51761893b95ac48e9c31db442e496eb
Amira <bcuba@unsa.edu.pe>
2025-10-28 02:46:06 -0500

    Ejercicio 19

---
+++ script17.js
@@ -0,0 +1,4 @@
+function mostrarDatos(nombre, edad, ...hobbies) {
+    alert(`Nombre: ${nombre} \nEdad: ${edad} \nHobbies: ${hobbies}`);
+}
+mostrarDatos("Amira", 18, "Leer", "Escuchar música", "Fotografía");
